ce9c823c86f688c6e9ad7b19c8b357c5dc03191f
Ola Bini <ola.bini@gmail.com>
2012-07-21 20:18:34 -0500

    Clean up implementation, and add some tests

--- src/clj_cas_client/core.clj
+++ src/clj_cas_client/core.clj
@@ -4,39 +4,67 @@
   (:require [clojure.tools.logging :as log])
   (:import (org.jasig.cas.client.validation Cas10TicketValidator
                                             TicketValidationException)))
+(def artifact-parameter-name "ticket")
+(def const-cas-assertion     "_const_cas_assertion_")
 
-(def ^{:private true} artifact-parameter-name "ticket")
-(def ^{:private true} const-cas-assertion "_const_cas_assertion_")
+(defprotocol Validator
+  (validate [v ticket service]))
 
-(defn- authentication-filter
+(extend-type Cas10TicketValidator
+  Validator
+  (validate [v ticket service] (.validate v ticket service)))
+
+(defn validator-maker [cas-server-fn]
+  (Cas10TicketValidator. (cas-server-fn)))
+
+(defn- valid? [request]
+  (or (get-in request [:session const-cas-assertion])
+      (get-in request [:params artifact-parameter-name])))
+
+(defn authentication-filter
   [handler cas-server-fn service-fn]
   (fn [request]
-    (if (or (get-in request [:session const-cas-assertion])
-            (get-in request [:params artifact-parameter-name]))
+    (if (valid? request)
       (handler request)
       (redirect (str (cas-server-fn) "/login?service=" (service-fn))))))
 
-(defn- set-session-var [res key val]
-  (assoc-in res [:session key] val))
+(defn session-assertion [res assertion]
+  (assoc-in res [:session const-cas-assertion] assertion))
 
-(defn- ticket-validation-filter
-  [handler cas-server-fn service-fn]
-  (let [ticket-validator (Cas10TicketValidator. (cas-server-fn))]
-    (fn [request]
-      (if-let [ticket (get-in request [:params artifact-parameter-name])]
-        (try
-          (let [assertion (.validate ticket-validator ticket (service-fn))]
-            (set-session-var (handler (update-in request [:params] assoc const-cas-assertion assertion))
-                             const-cas-assertion assertion))
-          (catch TicketValidationException e
-            (log/error "Ticket validation exception " e)
-            {:status 403}))
-        (handler request)))))
+(defn request-assertion [req assertion]
+  (update-in req [:params] assoc const-cas-assertion assertion))
+
+(defn ticket [r] (get-in r [:params artifact-parameter-name]))
+
+(defn ticket-validation-filter-maker [validator-maker]
+  (fn [handler cas-server-fn service-fn]
+    (let [ticket-validator (validator-maker cas-server-fn)]
+      (fn [request]
+        (if-let [t (ticket request)]
+          (try
+            (let [assertion (validate ticket-validator t (service-fn))]
+              (session-assertion (handler (request-assertion request assertion)) assertion))
+            (catch TicketValidationException e
+              (log/error "Ticket validation exception " e)
+              {:status 403}))
+          (handler request))))))
+
+(def ticket-validation-filter (ticket-validation-filter-maker validator-maker))
+
+(defn user-principal-filter [handler]
+  (fn [request]
+    (if-let [assertion (or (get-in request [:params const-cas-assertion])
+                           (get-in request [:session const-cas-assertion]))]
+      (handler (assoc request :username (.getName (.getPrincipal assertion))))
+      (handler request))))
 
 (defn cas
-  [handler cas-server-fn service-fn]
-  (-> handler
-      (authentication-filter cas-server-fn service-fn)
-      (ticket-validation-filter cas-server-fn service-fn)
-      ;; maybe adding username etc to request here
-      ))
+  [handler cas-server-fn service-fn & options]
+  (let [opts (apply hash-map options)]
+    (if (get opts :enabled true)
+      (-> handler
+          user-principal-filter
+          (authentication-filter cas-server-fn service-fn)
+          (ticket-validation-filter cas-server-fn service-fn)
+          )
+      handler)))
---
+++ test/clj_cas_client/core_test.clj
@@ -0,0 +1,58 @@
+(ns clj-cas-client.core-test
+  (:use clj-cas-client.core
+        clojure.test)
+  (:import (org.jasig.cas.client.validation Cas10TicketValidator
+                                            TicketValidationException
+                                            AssertionImpl)))
+
+(deftest constants-test
+  (is (= artifact-parameter-name "ticket"))
+  (is (= const-cas-assertion "_const_cas_assertion_")))
+
+(deftest validator-maker-test
+  (let [v (validator-maker (fn [] "foo bar"))]
+    (is (instance? Cas10TicketValidator v))))
+
+(deftest authentication-filter-test
+  (let [af (authentication-filter (fn [r] [:test-authentication-filter r]) (fn [] "cas server fn") (fn [] "service fn"))]
+    (is (= (af {:session {"_const_cas_assertion_" "blarg"}}) [:test-authentication-filter {:session {"_const_cas_assertion_" "blarg"}}]))
+    (is (= (af {:params {"ticket" "the tick"}}) [:test-authentication-filter {:params {"ticket" "the tick"}}]))
+    (is (= (af {:session {"_const_cas_assertion_" "blarg"}
+                :params {"ticket" "the tick"}}) [:test-authentication-filter {:session {"_const_cas_assertion_" "blarg"}
+                                                                              :params {"ticket" "the tick"}}]))
+    (is (= (af {}) {:status 302 :headers {"Location" "cas server fn/login?service=service fn"}  :body ""}))))
+
+(def ^:dynamic test-validator (fn [cas-server-fn ticket service] nil))
+
+(def test-ticket-validation-filter
+  (ticket-validation-filter-maker (fn [cas-server-fn]
+                                    (reify Validator
+                                      (validate [v ticket service]
+                                        (test-validator cas-server-fn ticket service))))))
+
+
+(deftest ticket-validation-filter-test
+  (let [tf (test-ticket-validation-filter (fn [r] {:res [:test-ticket-validation-filter r]}) (fn [] "cas server fn") (fn [] "service fn"))]
+    (is (= (tf {:nope 42}) {:res [:test-ticket-validation-filter {:nope 42}]}))
+    (binding [test-validator (fn [cas-server-fn ticket service]
+                               (is (= (cas-server-fn) "cas server fn"))
+                               (is (= ticket "ticket"))
+                               (is (= service "service fn"))
+                               :assertion)]
+      (is (= (tf {:params {"ticket" "ticket"}})
+             {:res [:test-ticket-validation-filter {:params {"ticket" "ticket"
+                                                             "_const_cas_assertion_" :assertion}}] :session {"_const_cas_assertion_" :assertion}})))
+
+    (binding [test-validator (fn [cas-server-fn ticket service] (throw (TicketValidationException. "blah")))]
+      (is (= (tf {:params {"ticket" "ticket"}})
+             {:status 403})))))
+
+(deftest user-principal-filter-test
+  (is (= ((user-principal-filter (fn [req] {:res [:user-principal-filter req]})) {:req 42}) {:res [:user-principal-filter {:req 42}]}))
+
+  (let [assertion (AssertionImpl. "foobar")]
+    (is (= ((user-principal-filter (fn [req] {:res [:user-principal-filter req]})) {:req 42 :params {"_const_cas_assertion_" assertion}})
+           {:res [:user-principal-filter {:username "foobar" :req 42 :params {"_const_cas_assertion_" assertion}}]}))
+    (is (= ((user-principal-filter (fn [req] {:res [:user-principal-filter req]})) {:req 42 :session {"_const_cas_assertion_" assertion}})
+           {:res [:user-principal-filter {:username "foobar" :req 42 :session {"_const_cas_assertion_" assertion}}]}))
+    ))
